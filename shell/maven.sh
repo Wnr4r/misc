2479583f4b87f8bb767c5ca96016563ddec969de
Tomisin <Wnr4r@users.noreply.github.com>
2022-12-26 08:59:14 +0200

    Create maven.sh

---
+++ shell/maven.sh
@@ -0,0 +1,11 @@
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.17.0.8-2.el8_6.x86_64/
+
+export M2_HOME=/usr/local/apache-maven
+
+export MAVEN_HOME=/usr/local/apache-maven
+
+export M2=$M2_HOME/bin
+
+export MAVEN_OPTS='-Xms256m -Xmx512m'
+
+export PATH=$M2:$PATH
